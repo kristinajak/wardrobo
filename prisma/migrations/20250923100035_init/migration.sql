298a5ae34afe3dd823c9d2de90796875030b6614
kristinajak <kristina.jakubone@gmail.com>
2025-09-23 13:09:19 +0300

    Add a seed scaffold

---
+++ prisma/migrations/20250923100035_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."ClothingItem" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ClothingItem_pkey" PRIMARY KEY ("id")
+);
